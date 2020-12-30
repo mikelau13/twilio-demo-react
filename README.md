37f2d258f0c5c2cce53f1d6e7c77d87590e2b2f9
mikelau13 <mikelau13@hotmail.com>
2020-12-30 14:29:44 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # twilio-demo-react
+
+## Running the application
+
+You will need your Twilio Account SID, API key and secret, available in [Twilio console](https://www.twilio.com/console), add them to the `.env` file.
+
+Then can run the application with:
+
+```bash
+npm run dev
+```
+
+This will open in your browser at [localhost:3000](http://localhost:3000).
---
+++ src/VideoChat.js
@@ -0,0 +1,64 @@
+import React, { useState, useCallback } from 'react';
+import Lobby from './Lobby';
+
+const VideoChat = () => {
+  const [username, setUsername] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [token, setToken] = useState(null);
+
+  const handleUsernameChange = useCallback(event => {
+    setUsername(event.target.value);
+  }, []);
+
+  const handleRoomNameChange = useCallback(event => {
+    setRoomName(event.target.value);
+  }, []);
+
+  /*
+  send the data as JSON to the endpoint, receive and parse the response, then use setToken to store the token in our state. 
+  */
+  const handleSubmit = useCallback(async event => {
+    event.preventDefault();
+    // route /video/token is in /server/index.js
+    const data = await fetch('/video/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: username,
+        room: roomName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json());
+    setToken(data.token);
+  }, [username, roomName]);
+
+  const handleLogout = useCallback(event => {
+    setToken(null);
+  }, []);
+
+  // render the Lobby unless we have a token; otherwise we'll render the username, roomName and token.
+  let render;
+  if (token) {
+    render = (
+      <div>
+        <p>Username: {username}</p>
+        <p>Room name: {roomName}</p>
+        <p>Token: {token}</p>
+      </div>
+    );
+  } else {
+    render = (
+      <Lobby
+         username={username}
+         roomName={roomName}
+         handleUsernameChange={handleUsernameChange}
+         handleRoomNameChange={handleRoomNameChange}
+         handleSubmit={handleSubmit}
+      />
+    );
+  }
+  return render;
+};
+
+export default VideoChat;
